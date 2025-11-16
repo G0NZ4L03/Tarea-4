bff99ff606d92b044a6b52f6cb49af30a6f2a68b
G0NZ4L03 <gonzalomarsau@gmail.com>
2025-11-16 19:57:26 +0100

    ejs 6 y 7 terminados y 8 tmb

--- Ejercicio 2/ejercicio2.js
+++ Ejercicio 2/ejercicio2.js
@@ -9,23 +9,58 @@ class banco {
         this.nombre = nombre;
         this.cuentas = {};
     }
-/*(7) Crear cuenta: Recibe como parámetros el código de cuenta y el saldo inicial(por defecto tomará el valor 0 si no se pasa el parámetro). */
 
-    CrearCuenta(codigoCuenta,saldoInicial) {
-        this.cuentas = {[codigoCuenta]: saldoInicial
+    // Creo esta funcion usando padStart para añadir 0 y cumplir la nomenclatura del enunciado 000001 al 599999 (lo hago con 6 digitos)
+    añadirDigitos(cod) {
+        return String(cod).padStart(6, "0");
+    }
+
+    /*(7) Crear cuenta: Recibe como parámetros el código de cuenta y el saldo inicial(por defecto tomará el valor 0 si no se pasa el parámetro). */
+
+    crearCuenta(codigoCuenta, saldoInicial) {
+        //le aplico la anterior funcion al numero recibido
+        codigoCuenta = this.añadirDigitos(codigoCuenta);
+        //hago la comprobacion del rango antes que nada y mando el error terminando la ejecucion
+        if (codigoCuenta < "000001" || codigoCuenta > "599999") {
+            return console.log("Error: El código de cuenta debe estar entre 000001 y 599999");
         }
-//TERMINAR
+        //si no intruducen saldo inicial, por defecto sera 0
+        if (!saldoInicial) {
+            saldoInicial = 0;
+        }
+        this.cuentas[codigoCuenta] = saldoInicial;
+    }
+
+    /*(8) Actualizar cuenta: Recibe como parámetros el código de cuenta y el ingreso(o extracción en negativo) y actualiza el saldo de la cuenta.Mostrará error si la cuenta no existe. */
+
+    actualizarCuenta(codCuenta, modificacionSaldo) {
+        if (!(codCuenta in this.cuentas)) {
+            return console.log("Error: La cuenta no existe");
+        }
+        this.cuentas[codCuenta] =  this.cuentas[codCuenta] + modificacionSaldo;
+    }
 }
-}
 
-let miBanco = new banco("Banco personal");
-miBanco.CrearCuenta("45567",345);
-console.log(miBanco.cuentas);
+let miBanco = new banco("Banco regional");
+//valido con saldo 0
+miBanco.crearCuenta("000123");
+//saldo inicial pero codigo fuera de rango
+miBanco.crearCuenta("600000", 200);
+//valido con saldo inicial
+miBanco.crearCuenta("599999", 1500);
+//añadiendo ceros al codigo
+miBanco.crearCuenta("45", 500);
 
+console.log("Ejercicios 6-7", miBanco.cuentas);
 
+//añado ingreso
+miBanco.actualizarCuenta("000123", 700);
+//quito saldo
+miBanco.actualizarCuenta("599999", -200);
+//codigo no existente
+miBanco.actualizarCuenta("123456", 200);
 
-
-/*(8) Actualizar cuenta: Recibe como parámetros el código de cuenta y el ingreso(o extracción en negativo) y actualiza el saldo de la cuenta.Mostrará error si la cuenta no existe. */
+console.log("Ejercicio 8", miBanco.cuentas);
 
 
 
@@ -35,5 +70,3 @@ console.log(miBanco.cuentas);
 
 /*(10) Listar cuentas: Escribe en el documento el nombre del banco y la lista de cuentas con sus saldos de forma similar a la mostrada en la imagen siguiente.
     Realiza 3 llamadas al método para crear cuentas, y una llamada a cada uno de los otros métodos para demostrar su funcionamiento..*/
-
-
